9aab293e0889606bb19021454292a66bc4f9cb36
Yevhenii Grytsay <60937148+sultan2131@users.noreply.github.com>
2022-12-29 09:54:19 +0200

    Add files via upload

---
+++ Lab6/Lab6/Lab6.cpp
@@ -0,0 +1,38 @@
+﻿#include <iostream> 
+using namespace std;
+struct Animals
+{
+    string name;
+    int age;
+    int weight;
+    string animal;
+    string poroda;
+};
+
+void printInformation(Animals a)
+{
+    cout << "Name: " << a.name << "\n";
+    cout << "Age: " << a.age << "\n";
+    cout << "Weight: " << a.weight << "\n";
+    cout << "Animal: " << a.animal << "\n";
+    cout << "Breed: " << a.poroda << "\n";
+}
+
+int main()
+{
+    Animals a;
+    cout << "Enter the name of pet\n";
+    cin >> a.name;
+    cout << "Enter the age of pet\n";
+    cin >> a.age;
+    cout << "Enter weight of pet\n";
+    cin >> a.weight;
+    cout << "Enter kind of animal of pet\n";
+    cin >> a.animal;
+    cout << "Enter breed of pet\n\n";
+    cin >> a.poroda;
+
+    printInformation(a);
+
+    return 0;
+}
